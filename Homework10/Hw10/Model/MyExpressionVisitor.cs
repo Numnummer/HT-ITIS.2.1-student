9cc623b95b4838ce14cb153cf966f5974a458cea
Akhmed Mirsaitov <bignumbergames@gmail.com>
2023-11-21 20:49:37 +0300

    HW-10 Вернул нормальную задержку

--- Homework10/Hw10/Model/MyExpressionVisitor.cs
+++ Homework10/Hw10/Model/MyExpressionVisitor.cs
@@ -13,7 +13,7 @@ namespace Hw10.Model
             var leftExpression = Task.Run(() => ProcessExpression(node.Left));
             var rightExpression = Task.Run(() => ProcessExpression(node.Right));
             var expressions = Task.WhenAll(leftExpression, rightExpression).Result;
-            Thread.Sleep(900);
+            Thread.Sleep(1000);
 
             lock (_lock)
             {
